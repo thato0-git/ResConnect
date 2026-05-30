27043df7ad5eb6806abadbef75bdb3b767058ff8
thato0-git <thatomokhuamathe@gmail.com>
2026-05-30 17:30:02 +0200

    final commit

--- mjs/accommodations.mjs
+++ mjs/accommodations.mjs
@@ -179,7 +179,7 @@ export const accommodations = [
         "description": "Sharing Apartment, Single Apartment",
         "university": "TUT",
         "funding": ["Bursary ", "Self Funded"],
-        "imageUrl": "images/accomodations/Ekhaya junction.jpg"
+        "imageUrl": "images/accomodations/Ekhaya residence.webp"
     },
     {
         "id": 21,    
